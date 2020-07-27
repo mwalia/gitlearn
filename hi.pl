5c4dc2f93c0cf13aaec24cf21d571d2b4cff6af3
mwalia <munish.walia@wipro.com>
2020-07-26 21:37:42 -0400

    POLL SCM

--- hi.pl
+++ hi.pl
@@ -1,4 +1,4 @@
 #! /usr/bin/perl -w
 print ("welcome to Devops PS \n") ;
 print ("Second lime added for newbranch \n") ; 
-print ("Jenkins INtegration 1 \n") ; 
+print ("Jenkins INtegration POLL SCM \n") ; 
